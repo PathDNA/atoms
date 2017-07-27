4dd34d9810ea3c9ce44617c879a50fab9ce602ec
itsMontoya <josh@montoya.io>
2017-07-27 11:05:49 -0700

    Added error re-use to test suite

---
+++ atomic_test.go
@@ -0,0 +1,6 @@
+package atomic
+
+const (
+	testErrInvalidValueFmt = "Invalid value, expected %d and received %d"
+	testErrInvalidSwapFmt  = "Swapped successfully when should have failed"
+)
--- int32_test.go
+++ int32_test.go
@@ -7,23 +7,23 @@ import (
 func TestInt32(t *testing.T) {
 	var i Int32
 	if val := i.Get(); val != 0 {
-		t.Fatalf("Invalid value, expected %d and received %d", 0, val)
+		t.Fatalf(testErrInvalidValueFmt, 0, val)
 	}
 
 	if val := i.Add(6); val != 6 {
-		t.Fatalf("Invalid value, expected %d and received %d", 6, val)
+		t.Fatalf(testErrInvalidValueFmt, 6, val)
 	}
 
 	if val := i.Add(7); val != 13 {
-		t.Fatalf("Invalid value, expected %d and received %d", 13, val)
+		t.Fatalf(testErrInvalidValueFmt, 13, val)
 	}
 
 	if val := i.Swap(3); val != 13 {
-		t.Fatalf("Invalid value, expected %d and received %d", 13, val)
+		t.Fatalf(testErrInvalidValueFmt, 13, val)
 	}
 
 	if i.CompareAndSwap(6, 13) {
-		t.Fatalf("Swapped successfully when should have failed")
+		t.Fatalf(testErrInvalidSwapFmt)
 	}
 
 	return
