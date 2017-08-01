a1dabb522337981649bdb21d43dd4a8ad4dd80b6
itsMontoya <josh@montoya.io>
2017-08-01 11:23:38 -0700

    Created proper main test func

--- atoms_test.go
+++ atoms_test.go
@@ -1,7 +1,20 @@
 package atoms
 
+import (
+	"os"
+	"testing"
+
+	"github.com/missionMeteora/journaler"
+)
+
 const (
 	testErrInvalidValueFmt = "Invalid value, expected %v and received %v"
-	testErrInvalidTypeFmt = "Invalid type, expected %T and received %T"
+	testErrInvalidTypeFmt  = "Invalid type, expected %T and received %T"
 	testErrInvalidSwapFmt  = "Swapped successfully when should have failed"
 )
+
+func TestMain(m *testing.M) {
+	journaler.Notification("Running atoms test suite")
+	sc := m.Run()
+	os.Exit(sc)
+}
